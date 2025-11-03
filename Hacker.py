4ce3f6768c56e2db287e8ae7b2db7b551a04e436
Jordan Harrison <orrotmg@gmail.com>
2025-11-03 19:43:36 +1030

    Added some trace level functionality

--- Hacker.py
+++ Hacker.py
@@ -26,6 +26,9 @@ class Hacker:
 
     def has_rig(self):
         return self.__rig
+    
+    def is_exposed(self):
+        return self.__trace_level >= 5
 
     def acquire_rig(self, rig_name):
         token = self.scan_inventory("CryptoToken")
@@ -37,7 +40,12 @@ class Hacker:
         # Create a new rig instance and assign it to the user
         self.__rig = Rig(rig_name)
         print(f"{self.__name} has activated rig '{rig_name}'!")
-        
+
+    def increase_trace(self, amount=1):
+        # Increases the trace level by either a specific amount or 1 if not provided.
+        self.__trace_level += amount
+        return
+
     def launch_attack(self, target_hacker):
         # Use a Data Spike to damage another hacker's rig
         pass
