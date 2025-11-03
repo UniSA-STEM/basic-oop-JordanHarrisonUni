70efd10fd7b13d802e271c51281a7efdfceac906
Jordan Harrison <orrotmg@gmail.com>
2025-11-03 19:26:53 +1030

    Added placeholder code and init method

--- Asset.py
+++ Asset.py
@@ -1,8 +1,35 @@
 """
 File: Asset.py
-Description: <A brief description of this Python module.>
+Description: A digital asset that can be stored and used by hackers/rigs
 Author: Jordan Daniel Harrison
 ID: harjd011
 Username: JordanHarrisonUni
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Asset:
+    # Initialisation method, takes parameters for name and description
+    def __init__(self, name, description):
+        self.__name = name
+        self.__description = description
+        self.__encrypted = False
+
+    # Getters
+    def get_name(self):
+        return self.__name
+
+    def is_encrypted(self):
+        return self.__encrypted
+
+    # Placeholder methods, will add implementation later
+    def encrypt(self):
+        # Mark asset as encrypted
+        pass
+
+    def decrypt(self):
+        # Mark asset as decrypted
+        pass
+
+    def __str__(self):
+        # Return a string showing name, description and encryption status
+        pass
